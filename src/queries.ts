c4edc7d11b8bcb2528e2fd3075f335def7fa1979
Bryce Anderson <abryce.anderson@gmail.com>
2023-03-07 13:05:37 -0500

    optimized styles query json_agg json_build_obj, refactored transform

--- src/queries.ts
+++ src/queries.ts
@@ -30,7 +30,7 @@ export const getProducts = (req: Request, res: Response) => {
 
   const pageNumber = (Number(req.params.page) * Number(req.params.count)).toString()
 
-  pool.query(`SELECT * FROM product WHERE id >= ${pageNumber} ORDER BY id LIMIT ${req.params.count}`)
+  pool.query(`SELECT * FROM product WHERE id >= $1 ORDER BY id LIMIT $2`, [pageNumber, req.params.count])
     .then((results) => {
       // console.log(results.rows)
       res.send(results.rows)
@@ -39,7 +39,7 @@ export const getProducts = (req: Request, res: Response) => {
 }
 
 export const getSingleProduct = (req: Request, res: Response) => {
-  pool.query(`SELECT product.id, product.name, product.slogan, product.description, product.category, product.default_price, features.feature, features.value FROM product INNER JOIN features ON product.id = features.product_id WHERE product.id = ${req.params.product_id}`)
+  pool.query(`SELECT product.id, product.name, product.slogan, product.description, product.category, product.default_price, features.feature, features.value FROM product INNER JOIN features ON product.id = features.product_id WHERE product.id = $1`, [req.params.product_id])
     .then((results) => {
       const transformedResults = transformProductRequest(results.rows)
       res.send(transformedResults)
@@ -53,20 +53,61 @@ export const getFeatures = (req: Request, res: Response) => {
     .catch((err) => {console.log(err)})
 }
 
+// export const getStyles = (req: Request, res: Response) => {
+//   pool.query(`SELECT * FROM styles INNER JOIN photos ON styles.id = photos.styles_id WHERE styles.product_id = $1`, [req.params.product_id])
+//     .then((query1) => {
+//       pool.query(`SELECT * FROM styles INNER JOIN skus ON styles.id = skus.styles_id WHERE styles.product_id = $1`, [req.params.product_id])
+//         .then((query2) => {
+//           const result = [];
+//           result.push(query1.rows);
+//           result.push(query2.rows);
+//           // console.log(result)
+//           res.send(transformStylesRequest(result));
+//         })
+//         .catch((err) => {console.log(err)});
+//     })
+//     .catch((err) => {console.log(err)});
+// }
+
 export const getStyles = (req: Request, res: Response) => {
-  pool.query(`SELECT * FROM styles INNER JOIN photos ON styles.id = photos.styles_id WHERE styles.product_id = ${req.params.product_id}`)
-    .then((query1) => {
-      pool.query(`SELECT * FROM styles INNER JOIN skus ON styles.id = skus.styles_id WHERE styles.product_id = ${req.params.product_id}`)
-        .then((query2) => {
-          const result = [];
-          result.push(query1.rows);
-          result.push(query2.rows);
-          // console.log(result)
-          res.send(transformStylesRequest(result));
-        })
-        .catch((err) => {console.log(err)});
+  const product_id = req.params.product_id;
+
+  pool.query(`
+    SELECT
+      json_agg(json_build_object(
+        'style_id', styles.id,
+        'name', styles.name,
+        'photos', (
+          SELECT json_agg(json_build_object(
+            'thumbnail_url', photos.thumbnail_url,
+            'url', photos.url
+          ))
+          FROM photos
+          WHERE photos.styles_id = styles.id
+        ),
+        'skus', (
+          SELECT json_object_agg(
+            skus.id, json_build_object(
+              'quantity', skus.quantity,
+              'size', skus.size
+            )
+          )
+          FROM skus
+          WHERE skus.styles_id = styles.id
+        )
+      )) as styles
+    FROM styles
+    WHERE styles.product_id = $1
+    GROUP BY styles.product_id
+  `, [product_id])
+    .then((query) => {
+      const result = query.rows[0];
+      res.json(result);
     })
-    .catch((err) => {console.log(err)});
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error retrieving styles data.');
+    });
 }
 
 export const getSkus = (req: Request, res: Response) => {
@@ -86,7 +127,7 @@ export const getPhotos = (req: Request, res: Response) => {
 
 
 export const getRelatedProducts = (req: Request, res: Response) => {
-  pool.query(`SELECT related_product_id FROM related WHERE current_product_id = ${req.params.product_id}`)
+  pool.query(`SELECT related_product_id FROM related WHERE current_product_id = $1`, [req.params.product_id])
     .then((results) => {
       // console.log(results.rows);
       res.send(transformRelated(results.rows));
--- src/transformHelpers.ts
+++ src/transformHelpers.ts
@@ -14,60 +14,82 @@ export const transformProductRequest = (rows) => {
 
 
 
+// export const transformStylesRequest = (rows) => {
+//   const stylesAndPhotos = rows[0];
+//   const stylesAndSkus = rows[1];
+//   // console.log(stylesAndSkus[0])
+//   const styles = {
+//     "product_id": Number(stylesAndSkus[0].product_id),
+//     "results": []
+//   };
+
+//   // console.log('stylesAndPhotos', stylesAndPhotos);
+//   for (let i = 0; i < stylesAndSkus.length; i++) {
+//     if (styles.results.filter(e => e.style_id === stylesAndSkus[i].styles_id).length > 0) {
+//       continue;
+//     } else {
+//       styles.results.push({
+//         "style_id": stylesAndSkus[i].styles_id,
+//         "name": stylesAndSkus[i].name,
+//         "original_price": stylesAndSkus[i].original_price + '.00',
+//         "sale_price": stylesAndSkus[i].sale_price,
+//         "default?": !!stylesAndSkus[i].default_style,
+//         "photos": [],
+//         "skus": {}
+//       })
+//     }
+//   }
+
+//   for (let i = 0; i < stylesAndSkus.length; i++) {
+//     for (let k = 0; k < styles.results.length; k++) {
+//       if (styles.results[k].style_id === stylesAndSkus[i].styles_id) {
+//         styles.results[k].skus[stylesAndSkus[i].id] = {
+//           "quantity": Number(stylesAndSkus[i].quantity),
+//           "size": stylesAndSkus[i].size
+//         }
+//       }
+//     }
+//   }
+
+//   for (let i = 0; i < stylesAndPhotos.length; i++) {
+//     for (let k = 0; k < styles.results.length; k++) {
+//       if (styles.results[k].style_id === stylesAndPhotos[i].styles_id) {
+//         const urlWithoutQuotes = stylesAndPhotos[i].url.slice(1, (stylesAndPhotos[i].url.length - 1))
+//         const thumbnailWithoutQuotes = stylesAndPhotos[i].thumbnail_url.slice(1, (stylesAndPhotos[i].thumbnail_url.length - 1))
+//         styles.results[k].photos.push({
+//           "thumbnail_url": thumbnailWithoutQuotes,
+//           "url": urlWithoutQuotes
+//         })
+//       }
+//     }
+//   }
+
+
+//   // console.log('stylesAndSkus', styles);
+//   return styles;
+// };
+
 export const transformStylesRequest = (rows) => {
-  const stylesAndPhotos = rows[0];
-  const stylesAndSkus = rows[1];
-  // console.log(stylesAndSkus[0])
+  const stylesAndData = rows[0];
+  if (!stylesAndData) {
+    return null;
+  }
+
   const styles = {
-    "product_id": Number(stylesAndSkus[0].product_id),
-    "results": []
+    "product_id": Number(stylesAndData[0].product_id),
+    "results": stylesAndData.map((style) => ({
+      "style_id": style.style_id,
+      "name": style.name,
+      "original_price": style.original_price + '.00',
+      "sale_price": style.sale_price,
+      "default?": !!style.default_style,
+      "photos": style.photos,
+      "skus": style.skus
+    }))
   };
 
-  // console.log('stylesAndPhotos', stylesAndPhotos);
-  for (let i = 0; i < stylesAndSkus.length; i++) {
-    if (styles.results.filter(e => e.style_id === stylesAndSkus[i].styles_id).length > 0) {
-      continue;
-    } else {
-      styles.results.push({
-        "style_id": stylesAndSkus[i].styles_id,
-        "name": stylesAndSkus[i].name,
-        "original_price": stylesAndSkus[i].original_price + '.00',
-        "sale_price": stylesAndSkus[i].sale_price,
-        "default?": !!stylesAndSkus[i].default_style,
-        "photos": [],
-        "skus": {}
-      })
-    }
-  }
-
-  for (let i = 0; i < stylesAndSkus.length; i++) {
-    for (let k = 0; k < styles.results.length; k++) {
-      if (styles.results[k].style_id === stylesAndSkus[i].styles_id) {
-        styles.results[k].skus[stylesAndSkus[i].id] = {
-          "quantity": Number(stylesAndSkus[i].quantity),
-          "size": stylesAndSkus[i].size
-        }
-      }
-    }
-  }
-
-  for (let i = 0; i < stylesAndPhotos.length; i++) {
-    for (let k = 0; k < styles.results.length; k++) {
-      if (styles.results[k].style_id === stylesAndPhotos[i].styles_id) {
-        const urlWithoutQuotes = stylesAndPhotos[i].url.slice(1, (stylesAndPhotos[i].url.length - 1))
-        const thumbnailWithoutQuotes = stylesAndPhotos[i].thumbnail_url.slice(1, (stylesAndPhotos[i].thumbnail_url.length - 1))
-        styles.results[k].photos.push({
-          "thumbnail_url": thumbnailWithoutQuotes,
-          "url": urlWithoutQuotes
-        })
-      }
-    }
-  }
-
-
-  // console.log('stylesAndSkus', styles);
   return styles;
-};
+}
 
 
 
